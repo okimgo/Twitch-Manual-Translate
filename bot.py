772ba21f0769b5f05a478ede6627616d72dc93f4
AlphaBs <ksi123456ab@naver.com>
2021-12-09 11:23:32 +0900

    Bot 클래스 분리, 파일 스트림 유지 성능 개선

---
+++ bot.py
@@ -0,0 +1,42 @@
+from twitchio.ext import commands
+import codecs
+from datetime import datetime
+
+class Bot(commands.Bot):
+    def __init__(self, twitchtoken: str, channel: str):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        self.channel_name = channel
+        super().__init__(token=twitchtoken, prefix='!', initial_channels=[channel])
+        self.file = codecs.open("chat.txt", 'w', 'utf-8')
+
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+
+        # Print the contents of our message to console...
+        #print(message.content)
+
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
+
+    @commands.command()
+    async def 번역(self, ctx: commands.Context):
+        if (ctx.author.is_mod or ctx.author.name == self.channel_name):
+            content = ctx.message.content[4:]
+
+            self.file.write("\n" + content)
+            self.file.flush()
+            print(content)
+    
+    def end(self):
+        self.file.close()
